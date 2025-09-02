cd23df945b1d82f3a5e328f873709eaf5a420f37
schlii <guy@guypersona.com>
2025-09-02 02:56:43 -0400

    removed presentation tab from sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,17 +7,12 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import { presentationTool } from 'sanity/presentation'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schema} from './src/sanity/schemaTypes'
 import {structure} from './src/sanity/structure'
 import {bandTheme} from './src/sanity/theme'
-import { env } from 'process'
-
-// derive site url (must not end with slash)
-const siteUrl = (env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000').replace(/\/$/, '')
 
 export default defineConfig({
   basePath: '/studio',
@@ -32,16 +27,5 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
-    // Presentation tool enables Visual Editing previews
-    presentationTool({
-      previewUrl: {
-        initial: siteUrl,
-        previewMode: {
-          enable: `${siteUrl}/api/draft/enable`,
-          disable: `${siteUrl}/api/draft/disable`,
-        },
-      },
-      allowOrigins: [siteUrl, 'http://localhost:*'],
-    }),
   ],
 })
